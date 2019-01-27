2a3988854eb1f271dc78e3b9bbee548feee20ecd
Juha-Matti Santala <juhamattisantala@gmail.com>
2019-01-27 12:38:24 +0200

    Manually add spring 2019 calendar because unit.run is bonkers

--- js/main.js
+++ js/main.js
@@ -1,35 +1,35 @@
-$(document).ready(function(){
-    $.get("https://meetabit.unit.run/events?turku-3-frontend", function(data, status){
-        for (var i = 0; i < data.results.length; i++) {
-            if (data.results[i]["past"] === true) {
-                if (i < 5) {
-                    $(".past").append(
-                        '<div>' +
-                        data.results[i]["date"] + ' <a href="' + data.results[i]["url"] + '">' + data.results[i]["name"] + '</a>' +
-                        '</div>'
-                    );
-                }
-            } else {
-                $(".upcoming").append(
-                    '<div>' +
-                    data.results[i]["date"] + ' <a href="' + data.results[i]["url"] + '">' + data.results[i]["name"] + '</a>' +
-                    '</div>'
-                );
-                $('.upcoming-manual').hide();
-            }
-        }
-    });
+// $(document).ready(function(){
+//     $.get("https://meetabit.unit.run/events?turku-3-frontend", function(data, status){
+//         for (var i = 0; i < data.results.length; i++) {
+//             if (data.results[i]["past"] === true) {
+//                 if (i < 5) {
+//                     $(".past").append(
+//                         '<div>' +
+//                         data.results[i]["date"] + ' <a href="' + data.results[i]["url"] + '">' + data.results[i]["name"] + '</a>' +
+//                         '</div>'
+//                     );
+//                 }
+//             } else {
+//                 $(".upcoming").append(
+//                     '<div>' +
+//                     data.results[i]["date"] + ' <a href="' + data.results[i]["url"] + '">' + data.results[i]["name"] + '</a>' +
+//                     '</div>'
+//                 );
+//                 $('.upcoming-manual').hide();
+//             }
+//         }
+//     });
 
-    $.get("https://mkaski.unit.run/medium-feed-cors", function(data, status){
-        let maxPosts = data.length > 5 ? 5 : data.length
-        for (var i = 0; i < maxPosts; i++) {
-            var date = new Date(Date.parse(data[i]["date"]));
-            date = date.toDateString();
-            $(".blog").append(
-                '<div>' +
-                date + ' <a href="' + data[i]["link"] + '">' + data[i]["title"] + '</a>' +
-                '</div>'
-            );
-        }
-    });
-});
+//     $.get("https://mkaski.unit.run/medium-feed-cors", function(data, status){
+//         let maxPosts = data.length > 5 ? 5 : data.length
+//         for (var i = 0; i < maxPosts; i++) {
+//             var date = new Date(Date.parse(data[i]["date"]));
+//             date = date.toDateString();
+//             $(".blog").append(
+//                 '<div>' +
+//                 date + ' <a href="' + data[i]["link"] + '">' + data[i]["title"] + '</a>' +
+//                 '</div>'
+//             );
+//         }
+//     });
+// });
